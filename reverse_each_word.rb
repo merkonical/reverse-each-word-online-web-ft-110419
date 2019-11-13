d38088f439f8da2709f46f9c90d4237b2890a27b
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 08:03:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  sentence.split.reverse_each do 
-    puts sentence
+  sentence.split.reverse_each do |reversed|
+    puts reversed 
   binding.pry 
   end 
 end 
